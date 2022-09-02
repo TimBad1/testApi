dc411f1ab3756634637aa7d87fc290d36bd360d8
Тим Бад <timurbadalbaev19@gmail.com>
2022-09-02 23:27:56 +0400

    добавлена функция создания таблицы

--- test/dist/test-0.1/test.js
+++ test/dist/test-0.1/test.js
@@ -1,34 +1,92 @@
-(function() {
-	function createTable() {
-		const table = document.createElement('table');
-		table.classList.add('table');
+(async function() {
+	let response = await fetch('http://testapi.r1i.lan-sevice.com/api/results/all', {
+		headers: { 'accept': 'application/json', 'App-Key': 'S0lk65CPBj5HEhbEGVWU2ZCgpzXl' },
+	});
 
-		let headerTable = createHeaderTable();
-    let mainTable = createMainTable();
+	let {items} = await response.json();
+	
+	console.log(items)
+	// function createTable() {
+	// 	const table = document.createElement('table');
+	// 	table.classList.add('table', 'table-bordered');
 
-		table.append(headerTable, mainTable);
-    return table;
-	}
+	// 	let headerTable = createHeaderTable();
+  //   let mainTable = createMainTable();
+
+	// 	table.append(headerTable, mainTable);
+  //   return table;
+	// }
 
 	function createHeaderTable() {
 
+		const  headItems = [
+			'№ в госреестре', 
+			'Наименование СИ', 
+			'Обозначение типа СИ', 
+			'Заводской №', 
+			'Год вып.', 
+			'Шифр', 
+			'Тип поверки', 
+			'Дата поверки', 
+			'Пригодность',
+		]
+
+		let head = document.createElement('thead');
+    let row = document.createElement('tr');
+
+		headItems.forEach(item => {
+
+			let heading = document.createElement('th');
+      heading.setAttribute('scope', 'col');
+
+      let title = document.createElement('h3');
+      title.classList.add('h5');
+      title.textContent = item;
+			heading.append(title);
+			row.append(heading);
+		})
+		head.append(row);
+		return head;
 	}
 
-	function createMainTable() {
+	function createItem(data) {
+		// console.log(data)
+		const item = {
+			number: data.vri_id, 									// 1
+			si_name: data.si_name, 								// 2
+			si_designation: data.si_designation, 	// 3
+			vri_id: data.certnotice,							// 4
+			certnotice: data.year,								// 5
+			sign: data.sign,											// 6
+			vritype_id: data.vritype_id,					// 7
+			date: data.verification_at,						// 8
+			applicability: data.applicability,		// 9
+		}
 
-	}
-
-	async function getData() {
-		let response = await fetch('http://testapi.r1i.lan-sevice.com/api/results/all', {
-      headers: { 'accept': 'application/json', 'App-Key': 'S0lk65CPBj5HEhbEGVWU2ZCgpzXl' },
-    });
+		const row = document.createElement('tr');
 		
-		let data = await response.json();
-		console.log(data);
+		for (let key in item) {
+			const td = document.createElement('td');
+			td.textContent = item[key];
+			row.append(td);
+		}
+		return row;
 	}
-	
-	let data = getData()
 
-	
+	function createMainTable(data) {
+		const body = document.createElement('tbody')
+
+		data.forEach(item => {
+			const row = createItem(item)
+			body.append(row);
+		})
+		return body;
+	}
+
+	let app = document.querySelector('#app-table');
+	app.classList.add('table', 'table-bordered', 'container');
+	app.append(createHeaderTable());
+	let main = createMainTable(items);
+	app.append(main);
 
 })()
